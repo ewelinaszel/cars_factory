a205fc89b67e7b3196b5f126964844731e45b312
e_szeliga <eszeliga@student.agh.edu.pl>
2021-11-28 12:18:21 +0100

    Stworzenie klasy Car

---
+++ Car.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Ewelina Szeliga on 27.11.2021.
+//
+
+#include "Car.h"
---
+++ Car.h
@@ -0,0 +1,14 @@
+//
+// Created by Ewelina Szeliga on 27.11.2021.
+//
+
+#ifndef FABRYKA_CAR_H
+#define FABRYKA_CAR_H
+
+
+class Car {
+
+};
+
+
+#endif //FABRYKA_CAR_H
